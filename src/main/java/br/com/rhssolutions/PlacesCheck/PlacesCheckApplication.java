63ce52d1ef5e6b4f804f4396127005beb425c3e8
RafaSamm <rafarhs286@gmail.com>
2023-09-19 21:57:05 -0300

    Project created with your layers

---
+++ src/main/java/br/com/rhssolutions/PlacesCheck/PlacesCheckApplication.java
@@ -0,0 +1,13 @@
+package br.com.rhssolutions.PlacesCheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacesCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacesCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rhssolutions/PlacesCheck/PlacesCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rhssolutions.PlacesCheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacesCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
